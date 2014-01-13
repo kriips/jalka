107d044054d990f8089ed9c85d5f5c2563adf1c0
Mikk Kard <mikk@heathmont.net>
2014-01-13 10:18:35 +0200

    submitting messages

--- client/views/application/messages_list.js
+++ client/views/application/messages_list.js
@@ -3,3 +3,16 @@ Template.messagesList.helpers({
 		return Messages.find();
 	}
 });
+
+Template.messagesList.events({
+	'submit form': function(e) {
+		e.preventDefault();
+		var message = {
+			author: Meteor.user().emails[0].address,
+			message: $(e.target).find('[name=message]').val(),
+			timestamp: new Date()
+		}
+		message._id = Messages.insert(message);
+		$(e.target).find('[name=message]').val('');
+	}
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,11 @@
 if (Messages.find().count() === 0) {
 	Messages.insert({
+		timestamp: '2014-01-01 17:10',
 		author: 'Mikk Test 1',
 		message: 'kõik on hästi'
 	});
 	Messages.insert({
+		timestamp: '2014-01-01 17:11',
 		author: 'Mikk Test 2',
 		message: 'kõik on ikka veel hästi'
 	});
