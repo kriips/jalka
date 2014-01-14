c57b038f2bea8e4e587e84bf70ff19b25befc747
Mikk Kard <mikk@heathmont.net>
2014-01-14 13:41:04 +0200

    submitting messages

--- client/views/application/messages_list.js
+++ client/views/application/messages_list.js
@@ -7,12 +7,19 @@ Template.messagesList.helpers({
 Template.messagesList.events({
 	'submit form': function(e) {
 		e.preventDefault();
-		var message = {
-			author: Meteor.user().emails[0].address,
-			message: $(e.target).find('[name=message]').val(),
-			timestamp: new Date()
+
+		if (Meteor.user()) {
+			var message = {
+				message: $(e.target).find('[name=message]').val(),
+			}
+			Meteor.call('addMessage', message, function(error, id) {
+				if (error)
+					Errors.throw(error.reason);
+			});
+			$(e.target).find('[name=message]').val('');
 		}
-		message._id = Messages.insert(message);
-		$(e.target).find('[name=message]').val('');
+		else {
+			Errors.throw("Logi palun sisse");
+		}
 	}
 });
