14074ab5019f34fa65f3e8d167a175f8b9bf3741
Mikk Kard <mikk@heathmont.net>
2014-04-04 00:12:19 +0300

    styling correction

--- client/views/application/home.js
+++ client/views/application/home.js
@@ -1,19 +1,31 @@
 Template.home.events({
-	'click .nav-pills': function(e) {
-//		e.preventDefault();
-		var target = $(e.target);
-		target.closest('li').addClass('active');
-		target.tab('show');
-		console.log(target.html());
+});
+
+Template.home.helpers({
+	activeTab: function(name) {
+		console.log(name);
+		console.log(Session.get('selectedTab'));
+		return name == Session.get('selectedTab') ? 'active' : '';
 	}
 });
 
 Template.home.rendered = function () {
 	console.log('home rendered');
+
 	if (Session.get('selectedCompetition').isPredictable) {
 		$('a[href="#predict"]').tab('show');
+		Session.set('selectedTab', 'predict');
 	}
 	else {
 		$('a[href="#watch"]').tab('show');
+		Session.set('selectedTab', 'watch');
 	}
+	$('#predictLink').click(function(e) {
+		console.log('predict clicked');
+		Session.set('selectedTab', 'predict');
+	});
+	$('#watchLink').click(function(e) {
+		console.log('watch clicked');
+		Session.set('selectedTab', 'watch');
+	});
 }
