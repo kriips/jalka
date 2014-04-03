d97b70fefde1df9a970caa7447e21c1f896473f3
Mikk Kard <mikk@heathmont.net>
2014-04-03 12:01:12 +0300

    iron router debugged

--- client/lib/router.js
+++ client/lib/router.js
@@ -4,6 +4,9 @@ Router.configure({
 	notFoundTemplate: 'notFound'
 });
 
+Router.onBeforeAction('loading')
+Router.onBeforeAction('dataNotFound')
+
 Router.map(function() {
 	this.route('home', {
 		path: '/',
@@ -33,15 +36,19 @@ Router.map(function() {
 		},
 		data: function() {
 			console.log('entering data');
-
-			var selectedCompetition = Competitions.findOne({url: this.params.url});
-			console.log(this.params.url);
-			Session.set('selectedCompetition', selectedCompetition);
-			document.title = 'S! jalkaennustus: ' + selectedCompetition.name;
-			var templateData = {
-				competition: Competitions.findOne({name: Session.get('selectedCompetition').name})
-			};
-			return templateData;
+			if (this.ready()) {
+				var selectedCompetition = Competitions.findOne({url: this.params.url});
+				console.log(this.params.url);
+				Session.set('selectedCompetition', selectedCompetition);
+				document.title = 'S! jalkaennustus: ' + selectedCompetition.name;
+				var templateData = {
+					competition: Competitions.findOne({name: Session.get('selectedCompetition').name})
+				};
+				return templateData;
+			}
+			else {
+				return false;
+			}
 		}
 	});
 });
